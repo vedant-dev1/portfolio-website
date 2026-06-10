c788ca977687bc5a30ff273a6e2ed38524750ae3
vedant dev <vedantdeo77@gmail.com>
2026-06-11 00:06:11 +0530

    home page created

---
+++ script.js
@@ -0,0 +1,38 @@
+const roles = [
+    "Python Developer",
+    "Graphic Designer",
+    "UI/UX Designer"
+];
+
+let roleIndex = 0;
+let charIndex = 0;
+
+const roleElement = document.getElementById("role");
+
+function typeEffect() {
+
+    if (charIndex < roles[roleIndex].length) {
+        roleElement.textContent += roles[roleIndex].charAt(charIndex);
+        charIndex++;
+        setTimeout(typeEffect, 100);
+    }
+    else {
+
+        setTimeout(() => {
+
+            roleElement.textContent = "";
+            charIndex = 0;
+            roleIndex++;
+
+            if (roleIndex >= roles.length) {
+                roleIndex = 0;
+            }
+
+            typeEffect();
+
+        }, 1500);
+
+    }
+}
+
+typeEffect();
